9ffdcdeee837f79f49192cf26823475478d0fc59
A01701088 <A01701088@itesm.mx>
2020-08-24 21:45:10 -0500

    Cuestionario Acabado

--- Lab 5/functions.js
+++ Lab 5/functions.js
@@ -113,19 +113,21 @@ function tienda(producto,operacion){
 function convertidorLibraKilo(){
     
     let cantidad = document.getElementById("cantidadConvertir").value;
-
-
-    if(document.getElementById("libra").checked){
-        cantidad = cantidad*2.204;
-        document.getElementById("resultado").innerHTML = cantidad + "lb"
+    if(isNaN(cantidad)){
+        alert("Por favor introduzca un numero");
     }
-    else if(document.getElementById("kilo").checked){
-        cantidad=cantidad*0.45392;
-        document.getElementById("resultado").innerHTML = cantidad + "kg"
+    else{
+        if(document.getElementById("libra").checked){
+            cantidad = cantidad*2.204;
+            document.getElementById("resultado").innerHTML = cantidad + "lb";
+        }
+        else if(document.getElementById("kilo").checked){
+            cantidad=cantidad*0.45392;
+            document.getElementById("resultado").innerHTML = cantidad + "kg";
+        }
+
+        console.log(cantidad);
+        console.log(document.getElementById("kilo").checked);
+        console.log(document.getElementById("libra").checked);
     }
-
-    console.log(cantidad);
-    console.log(document.getElementById("kilo").checked)
-    console.log(document.getElementById("libra").checked)
-
 }
