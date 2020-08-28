8aba7ff0b5b56f2812e11324903863d8332f558e
A01701088 <A01701088@itesm.mx>
2020-08-28 01:41:54 -0500

    Lab 6 finalizado

--- Lab 6/functions.js
+++ Lab 6/functions.js
@@ -6,6 +6,13 @@ function helpTextDissappear(){
     document.getElementById("help").style.display = "none";
 
 }
+//Cada veinte segundos la cantidad convertida se va a volver 0
+var timerCantidad = setInterval(clearCantidad,20000);
+
+function clearCantidad(){
+    document.getElementById("resultado").innerHTML = "Unidad Convertida:0"
+
+}
 
 
 
@@ -30,15 +37,36 @@ function convertidorLibraKilo(){
     else{
         if(document.getElementById("libra").checked){
             cantidad = cantidad*2.204;
-            document.getElementById("resultado").innerHTML = cantidad + "lb";
+            document.getElementById("resultado").innerHTML ="Unidad Convertida: "+ cantidad + "lb";
         }
         else if(document.getElementById("kilo").checked){
             cantidad=cantidad*0.45392;
-            document.getElementById("resultado").innerHTML = cantidad + "kg";
+            document.getElementById("resultado").innerHTML = "Unidad Convertida: "+  cantidad + "kg";
         }
 
         console.log(cantidad);
         console.log(document.getElementById("kilo").checked);
         console.log(document.getElementById("libra").checked);
     }
+}
+
+function dropAllow(x){
+    x.preventDefault();
+    
+
+
+}
+
+function drag(x){
+    x.dataTransfer.setData("text",x.target.id);
+
+}
+
+function drop(x){
+    x.preventDefault();
+    var xData= x.dataTransfer.getData("text");
+    x.target.appendChild(document.getElementById(xData));
+
+
+
 }
